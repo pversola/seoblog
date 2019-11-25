2fe53f36bad10b9a76c25393932410da63326925
Patrick Versola <pversola@gmail.com>
2019-11-25 20:07:15 +0700

    Database setup.

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
 
 // app
 const app = express();
+
+// db
+mongoose
+    .connect(`${process.env.DATABASE_LOCAL}`, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => { console.log('Database connected.') });
 
 // middlewares
 app.use(morgan('dev'));
