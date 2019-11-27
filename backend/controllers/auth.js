f17c74407e8f85cf7c1f490e9024b0e58b26de2d
patrickb <patrickb.v.chanwanich.com>
2019-11-27 10:05:26 +0700

    Add api signin & signout

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
     User.findOne({email: req.body.email}).exec((err, user) => {
@@ -30,3 +32,47 @@ exports.signup = (req, res) => {
         })
     });
 };
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body;
+
+    // check if user exist
+    User.findOne({email}).exec((err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: 'User with that email dose not exist. Please signup.'
+            });
+        }
+        
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match.'
+            });
+        }
+
+        // generate a token and send to client
+        const payload = { _id: user._id };
+        const options = { expiresIn: '1d' };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+
+        res.cookie('token', token, {expiresIn: '1d'});
+        const {_id, username, name, email, role} = user;
+        return res.json({
+            token,
+            user: {_id, username, name, email, role}
+        });
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie("token");
+    res.json({
+        message: 'Signout success'
+    });
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/auth');
+const { 
+    signup, 
+    signin, 
+    signout,
+    requireSignin 
+} = require('../controllers/auth');
 
 // validators
 const { runValidation } = require('../validators');
-const { userSignupValidator } = require('../validators/auth');
+const { 
+    userSignupValidator,
+    userSigninValidator 
+} = require('../validators/auth');
 
 router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/signin', userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+
+// test
+router.get('/secret', requireSignin, (req, res) => {
+    res.json({
+        message: 'you have access to secret page'
+    });
+});
 
 module.exports = router;
